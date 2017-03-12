04a7029a78fbf1fae699312a4da8487a643193a5
Nick Kowalchuk <kowalchn@Jasmine.local>
2017-03-12 16:28:45 -0600

    user model created with init function.

---
+++ app/VFlyer/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  VFlyer
+//
+//  Created by Nick Kowalchuk on 2017-03-12.
+//  Copyright © 2017 Mat Wilkie. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class User {
+    //MARK: Properties
+    var id: Int
+    
+    //MARK: Initialization
+    init?(id: Int) {
+        self.id = id
+    }
+}
