bdcbedca4a410d0482a74947d1e0ac5803873b19
Brayden Streibel <brayden@streibel.ca>
2017-03-25 14:47:02 -0600

    Integrated Promises, Fixed User

--- app/VFlyer/User.swift
+++ app/VFlyer/User.swift
@@ -13,13 +13,11 @@ class User {
     //MARK: Properties
     var _id: String?
     var userId: String
-    var radius: Int
-    var events = [Event]()
+    var radius: Int?
     
     //MARK: Initialization
-    init?(userId: String, radius: Int) {
+    init?(userId: String) {
         self.userId = userId
-        self.radius = radius
     }
     
     init?(json: [String: Any]) {
@@ -34,5 +32,42 @@ class User {
         self._id = _id;
         self.userId = userId;
         self.radius = radius;
+    }
+    
+    let BASE_URL = URL(string: "http://159.203.7.42:8000/api/users/")
+    
+    // MARK: Methods
+    public func login() -> Promise {
+        let p = Promise.defer()
+        
+        let url = URL(string: "login?fbUserId=\(userId)", relativeTo: BASE_URL)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+            
+            if let error = error {
+                // The error should bhe extracted from it's JSON dictionary and presented to the user.
+                print ("Problems upstream. Following errors occured: " + error.localizedDescription)
+                p.reject()
+            } else if let data = data {
+                let json = try? JSONSerialization.jsonObject(with: data, options: [])
+                if let response = json as? [String: Any] {
+                    self._id = response["_id"] as! String
+                    self.userId = response["userId"] as! String
+                    self.radius = response["radius"] as! Int
+                    
+                    p.resolve()()
+                } else {
+                    p.reject()
+                }
+            }
+        })
+        task.resume()
+        
+        return p
     }
 }
