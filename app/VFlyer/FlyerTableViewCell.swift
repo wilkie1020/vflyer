5be0639bce4c5384a693749979b2e3a1b99b83d2
Nick Kowalchuk <kowalchn@jasmine.gateway.sasktel.net>
2017-03-25 13:10:05 -0600

    IBOutlets added to FlyerTableViewCell

--- app/VFlyer/FlyerTableViewCell.swift
+++ app/VFlyer/FlyerTableViewCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class FlyerTableViewCell: UITableViewCell {
     //MARK: Properties
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var location: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
