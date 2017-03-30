2cd67610d8aba334ea2fedf594584107c6653a0a
Brayden Streibel <brayden@streibel.ca>
2017-03-29 23:14:44 -0600

    Im a dumbass

--- api/mongo_init.js
+++ api/mongo_init.js
@@ -2,8 +2,8 @@ var events = [
 {
     name: "Regina 1",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-01-01T00:00:00.000Z"),
+    endDate: new Date("2017-01-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.6189,
@@ -15,8 +15,8 @@ var events = [
 {
     name: "Regina 2",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.6180,
@@ -28,8 +28,8 @@ var events = [
 {
     name: "Regina 3",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.617,
@@ -41,8 +41,8 @@ var events = [
 {
     name: "Regina 4",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.62,
@@ -54,8 +54,8 @@ var events = [
 {
     name: "Tor Hill Golf Course",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.5,
@@ -67,8 +67,8 @@ var events = [
 {
     name: "Balgonie",
     description: "Event @ -104, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -104.268600,
@@ -80,8 +80,8 @@ var events = [
 {
     name: "Fort Qu'Appelle",
     description: "Event @ -103, 50",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -103.787150,
@@ -93,8 +93,8 @@ var events = [
 {
     name: "Saskatoon 1",
     description: "Event @ -106, 52",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -106.6700,
@@ -106,8 +106,8 @@ var events = [
 {
     name: "Saskatoon 2",
     description: "Event @ -106, 52",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -106.6750,
@@ -119,8 +119,8 @@ var events = [
 {
     name: "Saskatoon 3",
     description: "Event @ -106, 52",
-    startDate: "2017-06-01T00:00:00.000Z",
-    endDate: "2017-06-01T01:00:00.000Z",
+    startDate: new Date("2017-06-01T00:00:00.000Z"),
+    endDate: new Date("2017-06-01T01:00:00.000Z"),
     location: {
       coordinates: [
         -106.66,
